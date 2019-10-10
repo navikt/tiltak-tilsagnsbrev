2d6cf28791ca59af5389a58cf2a7af2fb6ccd439
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-10 12:37:47 +0200

    TAG-863 Deploy til preprod

--- src/main/java/no/nav/tag/tilsagnsbrev/TilsagnsbrevApplication.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/TilsagnsbrevApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TilsagnsbrevProsessApplication {
+public class TilsagnsbrevApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TilsagnsbrevProsessApplication.class, args);
+		SpringApplication.run(TilsagnsbrevApplication.class, args);
 	}
 
 }
