ac09889b708c03f0d957ec85c23d566a8668c487
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-16 11:58:06 +0200

    TAG-831 påbegynt MQ integrasjon

---
+++ src/main/java/no/nav/tag/tilsagnsbrev/dto/tilsagnsbrev/TiltakArrangor.java
@@ -0,0 +1,31 @@
+package no.nav.tag.tilsagnsbrev.dto.tilsagnsbrev;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TiltakArrangor {
+
+
+    private String arbgiverNavn;
+
+    private String kontoNummer;
+
+    private String landKode;
+
+    private String maalform;
+
+    private String orgNummer;
+
+    private String orgNummerMorselskap;
+
+    private String postAdresse;
+
+    private String postNummer;
+
+    private String postSted;
+
+}
