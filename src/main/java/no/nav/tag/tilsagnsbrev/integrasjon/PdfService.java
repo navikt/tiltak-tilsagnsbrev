d4ef3ad50372574136c0b23f1475d4ccd232d5ab
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-18 09:50:04 +0200

    pdf-skall

---
+++ src/main/java/no/nav/tag/tilsagnsbrev/integrasjon/PdfService.java
@@ -0,0 +1,15 @@
+package no.nav.tag.tilsagnsbrev.integrasjon;
+
+import no.nav.tag.tilsagnsbrev.dto.tilsagnsbrev.Tilsagn;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+
+
+    public byte[] tilsagnTilPdfBrev(Tilsagn tilsagn) {
+
+            //TODO Gjør ferdig
+            return "".getBytes();
+    }
+}
