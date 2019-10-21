e04458cb815271f1159f7d46fa6852fad0f9f989
Giao The Cung <giaothe@gmail.com>
2019-10-21 13:20:04 +0200

    Fix script.

--- export_service_user.sh
+++ export_service_user.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if test /secrets/serviceuser/srvtiltak-tilsagns/username
+if test -f /secrets/serviceuser/srvtiltak-tilsagns/username; 
 then
     export KAFKA_USERNAME=$(cat /secrets/serviceuser/srvtiltak-tilsagns/username)
 fi
 
-if test /secrets/serviceuser/srvtiltak-tilsagns/password
+if test -f /secrets/serviceuser/srvtiltak-tilsagns/password;
 then
     export KAFKA_PASSWORD=$(cat /secrets/serviceuser/srvtiltak-tilsagns/password)
 fi
