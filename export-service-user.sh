5fbf37d55bdbb06fbfd63077a7a7cb9b0245d086
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-22 09:40:40 +0200

    .

--- export-service-user.sh
+++ export-service-user.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
-if test -f /secrets/serviceuser/dev/srvtiltak-tilsagns/username;
+if test -f /secrets/serviceuser/srvtiltak-tilsagns/username;
 then
-    export KAFKA_USERNAME=H$(cat /secrets/serviceuser/dev/srvtiltak-tilsagns/username)
+    export KAFKA_USERNAME=H$(cat /secrets/serviceuser/srvtiltak-tilsagns/username)
 fi
 
-if test -f /secrets/serviceuser/dev/srvtiltak-tilsagns/password;
+if test -f /secrets/serviceuser/srvtiltak-tilsagns/password;
 then
-    export KAFKA_PASSWORD=$(cat /secrets/serviceuser/dev/srvtiltak-tilsagns/password)
+    export KAFKA_PASSWORD=$(cat /secrets/serviceuser/srvtiltak-tilsagns/password)
 fi
 
 if [ -z "$KAFKA_USERNAME" ] ;
 then
-    echo "KAFKA_USERNAME er ikke satt";
+    printf "KAFKA_USERNAME er ikke satt";
 else
-    echo "KAFKA_USERNAME er satt" ;
+    printf "KAFKA_USERNAME er satt" ;
 fi
 
 
