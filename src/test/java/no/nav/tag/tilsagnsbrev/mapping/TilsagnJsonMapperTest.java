389e3ef808331e9b02386c875de265668cb3ac80
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-23 16:48:36 +0200

    Konvertere kafka-melding til en json string

--- src/test/java/no/nav/tag/tilsagnsbrev/mapping/TilsagnJsonMapperTest.java
+++ src/test/java/no/nav/tag/tilsagnsbrev/mapping/TilsagnJsonMapperTest.java
@@ -14,71 +14,66 @@ public class TilsagnJsonMapperTest {
     @Test
     public void mapperGoldengateJsonTilTilsagn() {
 
-        String requestJson = Testdata.hentJsonFil();
+        String requestJson = Testdata.hentJsonFil(JSON_FIL_FEILER);
         Tilsagn tilsagn = tilsagnJsonMapper.goldengateJsonTilTilsagn(requestJson);
 
         assertEquals("tilsagnNummer.aar", "2019", tilsagn.getTilsagnNummer().getAar());
-        assertEquals("tilsagnNummer.loepenrSak", "319383", tilsagn.getTilsagnNummer().getLoepenrSak());
+        assertEquals("tilsagnNummer.loepenrSak", "366023", tilsagn.getTilsagnNummer().getLoepenrSak());
         assertEquals("tilsagnNummer.loepenrTilsagn", "1", tilsagn.getTilsagnNummer().getLoepenrTilsagn());
 
         assertEquals("tilsagnDato", TILSAGNSDATO, tilsagn.getTilsagnDato());
-        assertEquals("tiltakKode", "ENKFAGYRKE", tilsagn.getTiltakKode());
-        assertEquals("tiltakNavn", "Enkeltplass Fag- og yrkesopplæring VGS og høyere yrkesfaglig utdanning", tilsagn.getTiltakNavn());
-        assertEquals("administrasjonKode", "IND", tilsagn.getAdministrasjonKode());
+        assertEquals("tiltakKode", "BIO", tilsagn.getTiltakKode());
+        assertEquals("tiltakNavn", "Bedriftsintern opplæring (BIO)", tilsagn.getTiltakNavn());
+        assertEquals("administrasjonKode", "INST", tilsagn.getAdministrasjonKode());
 
         assertEquals("periode.fraDato", FRA_DATO, tilsagn.getPeriode().getFraDato());
         assertEquals("periode.tilDato", TIL_DATO, tilsagn.getPeriode().getTilDato());
 
-        assertEquals("arbgiverNavn", "TREIDER KOMPETANSE AS", tilsagn.getTiltakArrangor().getArbgiverNavn());
+        assertEquals("arbgiverNavn", "L.S. Solland AS", tilsagn.getTiltakArrangor().getArbgiverNavn());
         assertEquals("landKode", "NO", tilsagn.getTiltakArrangor().getLandKode());
-        assertEquals("postAdresse", "Nedre Vollgate 8", tilsagn.getTiltakArrangor().getPostAdresse());
-        assertEquals("postNummer", "0158", tilsagn.getTiltakArrangor().getPostNummer());
-        assertEquals("postSted", "OSLO", tilsagn.getTiltakArrangor().getPostSted());
-        assertEquals("orgNummerMorselskap", "920053106", tilsagn.getTiltakArrangor().getOrgNummerMorselskap());
-        assertEquals("orgNummer", "920130283", tilsagn.getTiltakArrangor().getOrgNummer());
-        assertEquals("kontoNummer", "32600596984", tilsagn.getTiltakArrangor().getKontoNummer());
+        assertEquals("postAdresse", "Pedersgata 110", tilsagn.getTiltakArrangor().getPostAdresse());
+        assertEquals("postNummer", "4014", tilsagn.getTiltakArrangor().getPostNummer());
+        assertEquals("postSted", "STAVANGER", tilsagn.getTiltakArrangor().getPostSted());
+        assertEquals("orgNummerMorselskap", "918160922", tilsagn.getTiltakArrangor().getOrgNummerMorselskap());
+        assertEquals("orgNummer", "973152289", tilsagn.getTiltakArrangor().getOrgNummer());
+        assertEquals("kontoNummer", "32010501481", tilsagn.getTiltakArrangor().getKontoNummer());
         assertEquals("maalform", "NO", tilsagn.getTiltakArrangor().getMaalform());
 
-        assertEquals("totaltTilskuddbelop", "59300", tilsagn.getTotaltTilskuddbelop());
+        assertEquals("totaltTilskuddbelop", "284000", tilsagn.getTotaltTilskuddbelop());
         assertEquals("valutaKode", "NOK", tilsagn.getValutaKode());
 
         assertTrue("tilskuddListe.TilskuddType", tilsagn.getTilskuddListe()
                 .stream()
-                .allMatch(tilskudd -> tilskudd.getTilskuddType().equals("Drift")));
+                .anyMatch(tilskudd -> tilskudd.getTilskuddType().equals("Opplæringstilskudd")));
+
+        assertTrue("tilskuddListe.TilskuddType", tilsagn.getTilskuddListe()
+                .stream()
+                .anyMatch(tilskudd -> tilskudd.getTilskuddType().equals("Lønnstilskudd")));
 
         assertTrue("tilskuddListe.tilskuddBelop", tilsagn.getTilskuddListe()
                 .stream()
-                .anyMatch(tilskudd ->
-                        tilskudd.getTilskuddBelop().equals("48000")
-                                || tilskudd.getTilskuddBelop().equals("1800")
-                                || tilskudd.getTilskuddBelop().equals("9500")));
+                .allMatch(tilskudd -> tilskudd.getTilskuddBelop().equals("142000")));
 
 
-        assertEquals("deltaker.fodselsnr", "0101011990", tilsagn.getDeltaker().getFodselsnr());
-        assertEquals("deltaker.fornavn", "OLA", tilsagn.getDeltaker().getFornavn());
-        assertEquals("deltaker.etternavn", "NORDMANN", tilsagn.getDeltaker().getEtternavn());
-        assertEquals("deltaker.landKode", "NO", tilsagn.getDeltaker().getLandKode());
-        assertEquals("deltaker.postAdresse", "Veien 1", tilsagn.getDeltaker().getPostAdresse());
-        assertEquals("deltaker.postNummer", "2013", tilsagn.getDeltaker().getPostNummer());
-        assertEquals("deltaker.postSted", "SKJETTEN", tilsagn.getDeltaker().getPostSted());
+        assertNull("deltaker", tilsagn.getDeltaker());
 
-        assertEquals("antallDeltakere", "1", tilsagn.getAntallDeltakere());
-        assertEquals("antallTimeverk", "120", tilsagn.getAntallTimeverk());
+        assertEquals("antallDeltakere", "17", tilsagn.getAntallDeltakere());
+        assertEquals("antallTimeverk", "2932", tilsagn.getAntallTimeverk());
 
-        assertEquals("navEnhet.navKontor", "0231", tilsagn.getNavEnhet().getNavKontor());
-        assertEquals("navEnhet.navKontorNavn", "NAV Skedsmo", tilsagn.getNavEnhet().getNavKontorNavn());
-        assertEquals("navEnhet.postAdresse", "Postboks 294", tilsagn.getNavEnhet().getPostAdresse());
-        assertEquals("navEnhet.postNummer", "2001", tilsagn.getNavEnhet().getPostNummer());
-        assertEquals("navEnhet.postSted", "LILLESTRØM", tilsagn.getNavEnhet().getPostSted());
+        assertEquals("navEnhet.navKontor", "1187", tilsagn.getNavEnhet().getNavKontor());
+        assertEquals("navEnhet.navKontorNavn", "NAV Tiltak Rogaland", tilsagn.getNavEnhet().getNavKontorNavn());
+        assertEquals("navEnhet.postAdresse", "Postboks 420", tilsagn.getNavEnhet().getPostAdresse());
+        assertEquals("navEnhet.postNummer", "4002", tilsagn.getNavEnhet().getPostNummer());
+        assertEquals("navEnhet.postSted", "STAVANGER", tilsagn.getNavEnhet().getPostSted());
         assertEquals("navEnhet.telefon", "55553333", tilsagn.getNavEnhet().getTelefon());
-        assertNull("navEnhet.faks", tilsagn.getNavEnhet().getFaks());
+        assertEquals("navEnhet.faks", "52048361", tilsagn.getNavEnhet().getFaks());
 
-        assertEquals("beslutter.fornavn", "Bjarne", tilsagn.getBeslutter().getFornavn());
-        assertEquals("beslutter.etternavn", "Beslutter", tilsagn.getBeslutter().getEtternavn());
+        assertEquals("beslutter.fornavn", "Evy", tilsagn.getBeslutter().getFornavn());
+        assertEquals("beslutter.etternavn", "Strømmen", tilsagn.getBeslutter().getEtternavn());
 
-        assertEquals("saksbehandler.fornavn", "Stine", tilsagn.getSaksbehandler().getFornavn());
-        assertEquals("saksbehandler.etternavn", "Saksbehandler", tilsagn.getSaksbehandler().getEtternavn());
+        assertEquals("saksbehandler.fornavn", "Odd Helge", tilsagn.getSaksbehandler().getFornavn());
+        assertEquals("saksbehandler.etternavn", "Johannessen", tilsagn.getSaksbehandler().getEtternavn());
 
-        assertEquals("kommentar", "Ingen", tilsagn.getKommentar());
+        assertNull("kommentar", tilsagn.getKommentar());
     }
 }
