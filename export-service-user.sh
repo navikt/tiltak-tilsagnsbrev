2825257765f3d965dce9978fd4eed20b5407726e
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-23 13:15:58 +0200

    Endret srvbruker prop

--- export-service-user.sh
+++ export-service-user.sh
@@ -2,28 +2,14 @@
 
 if test -f /secrets/serviceuser/srvtiltak-tilsagns/username;
 then
-    export KAFKA_USERNAME=$(cat /secrets/serviceuser/srvtiltak-tilsagns/username)
+    export tilsagnsbrev_srvuser=$(cat /secrets/serviceuser/srvtiltak-tilsagns/username)
 fi
 
 if test -f /secrets/serviceuser/srvtiltak-tilsagns/password;
 then
-    export KAFKA_PASSWORD=$(cat /secrets/serviceuser/srvtiltak-tilsagns/password)
+    export tilsagnsbrev_srvpwd=$(cat /secrets/serviceuser/srvtiltak-tilsagns/password)
 fi
 
-
-if [ -z "$KAFKA_USERNAME" ] ;
-then
-    printf "KAFKA_USERNAME er ikke satt \n";
-else
-    printf "KAFKA_USERNAME er ${KAFKA_USERNAME} \n" ;
-fi
-
-if [ -z "$KAFKA_PASSWORD" ] ;
-then
-    printf "KAFKA_PASSWORD er ikke satt \n";
-else
-    printf "KAFKA_PASSWORD er satt \n" ;
-fi
 
 
 
