b91c37b6124f179737fb8844c303cda1e7254ca1
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-28 13:48:00 +0100

    Mq conn i pp

--- src/main/java/no/nav/tag/tilsagnsbrev/mapping/TilsagnTilAltinnFormat.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/mapping/TilsagnTilAltinnFormat.java
@@ -1,9 +1,6 @@
 package no.nav.tag.tilsagnsbrev.mapping;
 
-import no.nav.tag.tilsagnsbrev.dto.altinn.header.BusinessScope;
-import no.nav.tag.tilsagnsbrev.dto.altinn.header.BusinessService;
-import no.nav.tag.tilsagnsbrev.dto.altinn.header.Scope;
-import no.nav.tag.tilsagnsbrev.dto.altinn.header.StandardBusinessDocumentHeader;
+import no.nav.tag.tilsagnsbrev.dto.altinn.header.*;
 import no.nav.tag.tilsagnsbrev.dto.tilsagnsbrev.Tilsagn;
 
 public class TilsagnTilAltinnFormat {
@@ -23,12 +20,15 @@ public class TilsagnTilAltinnFormat {
         scope.setInstanceIdentifier("unik"); //TODO hvordan generere
         scope.setBusinessService(new BusinessService("ALTINNRapportere",""));  //TODO xzmlgen m/attr
 
-
         BusinessScope businessScope = new BusinessScope();
         businessScope.getScope().add(scope);
         header.setBusinessScopeObject(businessScope);
 
-        //TODO gjør ferdig
+        header.setDocumentIdentificationObject(new DocumentIdentification(tilsagn.getTilsagnDato().toString(), "INstanceId"));
+
+        header.setReceiverObject(new Receiver(tilsagn.getTiltakArrangor().getOrgNummer()));
+        header.setSenderObject(new Sender(tilsagn.getNavEnhet().getNavKontor()));
+
         return header;
     }
 }
