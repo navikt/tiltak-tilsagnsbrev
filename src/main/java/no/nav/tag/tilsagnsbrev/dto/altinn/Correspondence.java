51f17bfa6b82abeae031196309fd3b16d2a524fd
Bjarte Tynning <bjarte.tynning@nav.no>
2019-10-29 15:58:00 +0100

    Mer altinn mapping

--- src/main/java/no/nav/tag/tilsagnsbrev/dto/altinn/Correspondence.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/dto/altinn/Correspondence.java
@@ -4,16 +4,20 @@ import lombok.Data;
 
 @Data
 public class Correspondence {
-    String ServiceCode;
-    String ServiceEdition;
-    String Reportee; //lik receier u. kommunenr
-    Content Content;
-    String AllowSystemDeleteDateTimeObject;
-    String ArchiveReferenceObject;
-    String ReplyOptionsObject;
-    String NotificationsObject;
-    String AllowForwardingObject;
-    String CaseIDObject;
-    String MessageSenderObject;
-    String IsReservableObject;
+
+    private static final String SERVICE_CODE = "0000"; //TODO finn servicekode
+    private static final String SERVICE_EDITION = "1"; //TODO finn service-edition
+
+    private final String ServiceCode = SERVICE_CODE;
+    private final String ServiceEdition = SERVICE_EDITION;
+    private String Reportee; //lik receier u. kommunenr
+    //private String AllowSystemDeleteDateTimeObject;
+    //private String ArchiveReference;
+//    private String ReplyOptions;
+//    private String Notifications;
+//    private String AllowForwardingObject;
+//    private String CaseIDObject;
+//    private String MessageSenderObject;
+//    private String IsReservableObject;
+    private Content Content;
 }
