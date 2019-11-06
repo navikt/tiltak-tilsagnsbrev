302f7a8ab9a634749e30fc27d30095cd307fe916
Bjarte Tynning <bjarte.tynning@nav.no>
2019-11-06 11:59:09 +0100

    profilsjekk

--- src/main/java/no/nav/tag/tilsagnsbrev/TilsagnsbrevApplication.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/TilsagnsbrevApplication.java
@@ -2,12 +2,16 @@ package no.nav.tag.tilsagnsbrev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TilsagnsbrevApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TilsagnsbrevApplication.class, args);
+		SpringApplication application = new SpringApplicationBuilder(TilsagnsbrevApplication.class)
+				.initializers(new SjekkAktiveProfilerInitializer())
+				.build();
+		application.run(args);
 	}
 
 }
