b41f5a87fc3e6910f846164cac80f738794dd92b
bjartty <bjarte.tynning@nav.no>
2019-12-20 12:00:39 +0100

    Lagt til en test

--- src/test/java/no/nav/tag/tilsagnsbrev/integrasjon/JoarkServiceTest.java
+++ src/test/java/no/nav/tag/tilsagnsbrev/integrasjon/JoarkServiceTest.java
@@ -3,7 +3,6 @@ package no.nav.tag.tilsagnsbrev.integrasjon;
 import no.nav.tag.tilsagnsbrev.dto.journalpost.*;
 import no.nav.tag.tilsagnsbrev.integrasjon.sts.StsService;
 import no.nav.tag.tilsagnsbrev.konfigurasjon.JoarkKonfig;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -22,12 +21,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class JoarkServiceTest {
 
     private final URI uri = URI.create("http://localhost:8090");
     private final URI expUri = UriComponentsBuilder.fromUri(uri).path(PATH).query(QUERY_PARAM).build().toUri();
+
+    private final Journalpost journalpost =
+            new Journalpost("Tittel", new Bruker("bid"), new Mottaker("mid", "navn"), new Sak("fagsakId"), Arrays.asList());
 
     @Mock
     private RestTemplate restTemplate;
@@ -40,29 +41,29 @@ public class JoarkServiceTest {
 
     @Test
     public void kall_mot_joark_ok_skal_returnere_journalpostid() {
-        Journalpost journalpost =
-                new Journalpost("Tittel", new Bruker("bid"),new Mottaker("mid", "navn"), new Sak("fagsakId"), Arrays.asList());
-
         JournalpostResponse joarkResponse = new JournalpostResponse();
         joarkResponse.setJournalpostId("123");
         when(restTemplate.postForObject(eq(expUri), any(HttpEntity.class), any())).thenReturn(joarkResponse);
         joarkService.sendJournalpost(journalpost);
-        verify(restTemplate, times(1)).postForObject(anyString(), any(HttpEntity.class), any());
+        verify(restTemplate, times(1)).postForObject(eq(expUri), any(HttpEntity.class), any());
     }
-    
+
     @Test(expected = RuntimeException.class)
     public void oppretterJournalpost_status_500() {
         when(restTemplate.postForObject(eq(expUri), any(HttpEntity.class), any())).thenThrow(RuntimeException.class);
-        //joarkService.opprettOgSendJournalpost() journalfoerTilsagnsbrev(new Journalpost());
+        joarkService.sendJournalpost(null);
     }
 
     @Test
     public void feil_mot_tjeneste_skal_hente_nytt_sts_token_og_forsøke_på_nytt() {
-//        when(restTemplate.postForObject(eq(expUri), any(HttpEntity.class), any())).thenThrow(RuntimeException.class).thenReturn(new JoarkResponse());
-    //    joarkService.journalfoerTilsagnsbrev(new Journalpost());
-        verify(stsService).evict();
-        verify(stsService, times(2)).hentToken();
-        verify(restTemplate, times(2)).postForObject(eq(expUri), any(HttpEntity.class), any());
+        when(restTemplate.postForObject(eq(expUri), any(HttpEntity.class), any())).thenThrow(RuntimeException.class);
+        try {
+            joarkService.sendJournalpost(journalpost);
+        } catch (RuntimeException re) {
+            verify(stsService).evict();
+            verify(stsService, times(2)).hentToken();
+            verify(restTemplate, times(2)).postForObject(eq(expUri), any(HttpEntity.class), any());
+        }
     }
 
 }
