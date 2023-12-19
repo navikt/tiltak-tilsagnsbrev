d4cec43c84888ba23f6bb57d8afeb2bb7cb7b3f6
Sindre Dahl Løken <sindre.loken@nav.no>
2023-12-19 15:27:26 +0100

    bytter til tilsagnsbrevId som eksternReferanseId

--- src/main/java/no/nav/tag/tilsagnsbrev/mapper/TilsagnJournalpostMapper.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/mapper/TilsagnJournalpostMapper.java
@@ -27,21 +27,22 @@ public class TilsagnJournalpostMapper {
 
     public Journalpost tilsagnTilJournalpost(TilsagnUnderBehandling tilsagnUnderBehandling) {
         try {
-            return opprettJournalpost(tilsagnUnderBehandling.getTilsagn(), tilsagnUnderBehandling.getPdf());
+            return opprettJournalpost(tilsagnUnderBehandling.getTilsagn(), tilsagnUnderBehandling.getPdf(), tilsagnUnderBehandling.getTilsagnsbrevId());
         } catch (Exception e) {
             log.error("Feil ved mapping til Journalpost", e);
             throw new DataException(e.getMessage());
         }
     }
 
-    private Journalpost opprettJournalpost(Tilsagn tilsagnsbrev, final byte[] pdfBytes) {
+    private Journalpost opprettJournalpost(Tilsagn tilsagnsbrev, final byte[] pdfBytes, Integer tilsagnsbrevId) {
+        String eksternReferanseId = tilsagnsbrevId + "-tilsagnsbrev";
         Sak sak = new Sak(opprettArkivsaknr(tilsagnsbrev.getTilsagnNummer()));
         Bruker bruker = new Bruker(tilsagnsbrev.getTiltakArrangor().getOrgNummer());
         Mottaker mottaker = new Mottaker(tilsagnsbrev.getTiltakArrangor().getOrgNummer(), tilsagnsbrev.getTiltakArrangor().getArbgiverNavn());
         final String base64EncodetPdf = Base64.getEncoder().encodeToString(pdfBytes);
         TiltakType tiltakType = getTiltakType(tilsagnsbrev.getTiltakKode());
         Dokument dokument = new Dokument(tiltakType.getTittel(), tiltakType.getBrevkode(), Collections.singletonList(new DokumentVariant(base64EncodetPdf)));
-        Journalpost journalpost = new Journalpost(tiltakType.getTittel(), bruker, mottaker, sak, Collections.singletonList(dokument), sak.getFagsakId());
+        Journalpost journalpost = new Journalpost(tiltakType.getTittel(), bruker, mottaker, sak, Collections.singletonList(dokument), eksternReferanseId);
         return journalpost;
     }
 
