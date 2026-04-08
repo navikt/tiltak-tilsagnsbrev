2eea06b36c4b9b8f3ac89abf10004ef9734a1052
Eirik Vigeland <eirik.vigeland@nav.no>
2026-04-08 14:05:46 +0200

    fix. utvidet SystemException med thorwable

--- src/main/java/no/nav/tag/tilsagnsbrev/exception/SystemException.java
+++ src/main/java/no/nav/tag/tilsagnsbrev/exception/SystemException.java
@@ -5,4 +5,8 @@ public class SystemException extends TilsagnException {
     public SystemException(String errMsg) {
         super(errMsg, false);
     }
+
+    public SystemException(String errMsg, Throwable cause) {
+        super(errMsg, false, cause);
+    }
 }
